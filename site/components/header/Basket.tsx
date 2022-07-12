1df1ab4cc96d7b145ae533a0a2f27ddbe25b29f8
nextjspractika <nextjs.practika@gmail.com>
2022-07-12 19:10:09 +0300

    fixed svg images

--- site/components/header/Basket.tsx
+++ site/components/header/Basket.tsx
@@ -12,7 +12,7 @@ export function Basket(props:BasketProps){
         
         <div className={classes.basket}>            
             <svg height="50" width="50">
-                <circle cx="25" cy="25" r="20" stroke='#FFEFCC' stroke-width="3" fill="#feebc0" />               
+                <circle cx="25" cy="25" r="20" stroke='#FFEFCC'  fill="#feebc0" />               
             </svg>
             <IconBasket/>
             <p>{props.cartNumber}</p>
