f7ac0f64ee34be25c0911381458bca64fec939be
Harmary <maria.h4rlamova@yandex.ru>
2022-07-14 18:47:54 +0300

    Сделана кнопка на карточке

--- site/components/shop/Card/index.tsx
+++ site/components/shop/Card/index.tsx
@@ -1,8 +1,11 @@
 import classes from './card.module.css'
 import useClassName from '../../../hooks/useClassName'
-import CartButton from '../CartButton'
 import type { Product } from '@commerce/types/product'
 import Image from 'next/image'
+import { useState } from 'react'
+import { useAddItem } from '@framework/cart'
+import CartIcon from '../../icons/IconBasket'
+
 
 type TProps = {
   className?: string
@@ -11,6 +14,20 @@ type TProps = {
 }
 
 export function Card(props: TProps) {
+  const [loading, setLoading] = useState(false)
+  const addItem = useAddItem()
+  const addToCart = async () => {
+    setLoading(true)
+    try {
+      await addItem({
+        productId: String(props.product.id),
+        variantId: String(props.product.variants[0]?.id),
+      })
+      setLoading(false)
+    } catch (err) {
+      setLoading(false)
+    }
+  }
   const { isCardList = true, className } = props
   const cn = useClassName()
   return (
@@ -19,7 +36,12 @@ export function Card(props: TProps) {
         [classes['card__list']]: isCardList,
       })}
     >
-      <CartButton />
+      <button
+      className={classes['cart-button']}
+      onClick={addToCart}
+    >
+    <CartIcon />
+    </button>
       <div className={classes.card__img}>
         <Image
            height={200}
