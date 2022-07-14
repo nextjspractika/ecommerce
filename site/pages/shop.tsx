06c2076dae5369c0194622eb9d2730dfe8460c18
Harmary <maria.h4rlamova@yandex.ru>
2022-07-14 16:55:46 +0300

    Исправлен Layout

--- site/pages/shop.tsx
+++ site/pages/shop.tsx
@@ -1,30 +1,10 @@
 import CardList from '@components/shop/CardList'
-import Footer from '@components/footer'
 import React from 'react'
-import { useState } from 'react';
-import { Layout } from '@components/layout'
-import {Navbar} from '@components/common';
+import { Layout } from '@components/common'
 
-export function Shop() {
-  let num =0;
-  let price =0;
-  const [count, setCount] = useState(num);
-  const [totalPrice, setTotalPrice] = useState(price);
-
- 
-  /*let cartNumber =
-    typeof window !== 'undefined' ? localStorage.getItem('cartNumber') : null
-  let totalPrice =
-    typeof window !== 'undefined' ? localStorage.getItem('totalPrice') : null
-  if (cartNumber == null) cartNumber = '0'
-  if (totalPrice == null) totalPrice = '0'
-*/
+export default function Shop() {
   return (
-    <Layout cartNumber={count} totalPrice={totalPrice}>
-      <Navbar/>
       <CardList />
-      <Footer />
-    </Layout>
   )
 }
-export default Shop
+Shop.Layout = Layout
