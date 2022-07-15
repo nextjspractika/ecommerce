b86d345568e88ca6600cae48073b65f34df08ad9
nextjspractika <nextjs.practika@gmail.com>
2022-07-15 16:31:13 +0300

    слайдер но на js

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -4,7 +4,8 @@ import { ProductCard } from '@components/product'
 import { Grid, Marquee, Hero } from '@components/ui'
 // import HomeAllProductsGrid from '@components/common/HomeAllProductsGrid'
 import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next'
-
+import CarouselCompound from '@components/slider/carousel-compound'
+import classes from './index.module.css';
 export async function getStaticProps({
   preview,
   locale,
@@ -39,52 +40,22 @@ export default function Home({
   products,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <>
-      <Grid variant="filled">
-        {products.slice(0, 3).map((product: any, i: number) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-            imgProps={{
-              width: i === 0 ? 1080 : 540,
-              height: i === 0 ? 1080 : 540,
-              priority: true,
-            }}
-          />
-        ))}
-      </Grid>
-      <Marquee variant="secondary">
-        {products.slice(0, 3).map((product: any, i: number) => (
-          <ProductCard key={product.id} product={product} variant="slim" />
-        ))}
-      </Marquee>
-      <Hero
-        headline=" Dessert dragée halvah croissant."
-        description="Cupcake ipsum dolor sit amet lemon drops pastry cotton candy. Sweet carrot cake macaroon bonbon croissant fruitcake jujubes macaroon oat cake. Soufflé bonbon caramels jelly beans. Tiramisu sweet roll cheesecake pie carrot cake. "
-      />
-      <Grid layout="B" variant="filled">
-        {products.slice(0, 3).map((product: any, i: number) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-            imgProps={{
-              width: i === 0 ? 1080 : 540,
-              height: i === 0 ? 1080 : 540,
-            }}
-          />
-        ))}
-      </Grid>
-      <Marquee>
-        {products.slice(3).map((product: any, i: number) => (
-          <ProductCard key={product.id} product={product} variant="slim" />
-        ))}
-      </Marquee>
-      {/* <HomeAllProductsGrid
-        newestProducts={products}
-        categories={categories}
-        brands={brands}
-      /> */}
-    </>
+    
+    
+    <CarouselCompound infinite>
+      <CarouselCompound.Page>
+        <div className={`${classes.item} ${classes.item1}`}>Item 1</div>
+      </CarouselCompound.Page>
+      <CarouselCompound.Page>
+        <div className={`${classes.item} ${classes.item2}`}>Item 2</div>
+      </CarouselCompound.Page>
+      <CarouselCompound.Page>
+        <div className={`${classes.item} ${classes.item3}`}>Item 3</div>
+      </CarouselCompound.Page>
+      </CarouselCompound>
+      
+      
+    
   )
 }
 
